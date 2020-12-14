971ec4b4a25606d8ba48a3ce274e6aaa763ba5c4
Ernestina77 <69101468+Ernestina77@users.noreply.github.com>
2020-12-13 18:48:47 -0600

    Primer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <HeaderVue />
+    <ContenedorVue/>
+    <PieVue/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu'
+import HeaderVue from './components/HeaderVue'
+import ContenedorVue from './components/ContenedorVue'
+import PieVue from './components/PieVue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
+    HeaderVue,
+    ContenedorVue,
+    PieVue,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
